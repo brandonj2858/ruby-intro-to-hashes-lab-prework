b1f3ed49276cf3ece8ddb4bac5d3003be0eb1ae7
Brandon James <brandonj2858@gmail.com>
2019-07-11 18:27:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
 	    :reading_railroad => {},
 	    :pennsylvania_railroad => {},
 	    :b_and_o_railroad => {},
-	    :shortline => {}
+	    :shortline => {}}
 	  }
 	  :rent_in_dollars => {
 	    :one_piece_owned => 25,
@@ -47,6 +47,6 @@ def monopoly_with_fourth_tier
 	
 end
 
-
+end
 
 
