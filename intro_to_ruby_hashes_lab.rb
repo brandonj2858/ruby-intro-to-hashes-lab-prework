1b78aadc37f6c4dc79250c723dbc6ea68a106f3b
Brandon James <brandonj2858@gmail.com>
2019-07-11 18:55:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,8 +32,7 @@ def monopoly_with_third_tier
 	    :four_pieces_owned => 200
 	  },
 	  :names => {
-	    :reading_railroad => {
-	      "mortgage_value" => "$100"},
+	    :reading_railroad => {},
 	    :pennsylvania_railroad => {
 	      "mortgage_value" => "$200"
 	    },
@@ -60,7 +59,7 @@ def monopoly_with_fourth_tier
 	    :four_pieces_owned => 200
 	  },
 	  :names => {
-	    :reading_railroad => {},
+	    :reading_railroad => {"mortgage_value" => "$100"},
 	    :pennsylvania_railroad => {},
 	    :b_and_o_railroad => {}
 	}
